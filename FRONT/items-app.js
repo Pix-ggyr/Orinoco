565ddf7a05a806bc3906aaa4aa8b760338891f50
Pix-ggyr <gwenaelle.goyer@gmail.com>
2020-06-25 10:21:40 +0200

    un peu de rangement

--- FRONT/items-app.js
+++ FRONT/items-app.js
@@ -1,3 +1,7 @@
+const srcAPI = "http://localhost:3000/api/furniture/";
+
+    //GET products list
+
 function displayItems() {
     var searchItems = new XMLHttpRequest();
     searchItems.onreadystatechange = function() {
@@ -17,8 +21,32 @@ function displayItems() {
             })
         }
     };
-    searchItems.open("GET" , "http://localhost:3000/api/furniture/");
+    searchItems.open("GET" , srcAPI);
     searchItems.send();
 }
-
 displayItems();
+
+
+    // GET furnitures details one per page
+
+const productDetails = document.getElementById("product-details");
+const itemPath = window.location.search;
+console.log(itemPath);
+    //?id=5be9cc611c9d440000c1421e - CROSS TABLE
+    //?id=5beaadda1c9d440000a57d98 - COFFEE TABLE
+    //?id=5beaae361c9d440000a57d99 - DINING TABLE 
+    //?id=5beaaf2e1c9d440000a57d9a - BENCH
+    //?id=5beab2061c9d440000a57d9b - VINTAGE CHAIR
+
+function itemDetails() {
+    var searchProduct = new XMLHttpRequest();
+    if (searchProduct.readyState == XMLHttpRequest.DONE && searchItems.status == 200) {
+        let itemDetails = JSON.parse(searchProduct.response);
+        console.log("itemDetails");
+    }
+
+    searchProduct.open("GET" , srcAPI);
+    searchProduct.send();
+}
+
+itemDetails();
