b2b7a9613f329baae2c7163b23424a4ce6070472
Pix-ggyr <gwenaelle.goyer@gmail.com>
2020-07-10 13:02:24 +0200

    add to cart fixed

--- FRONT/JS/product-details.js
+++ FRONT/JS/product-details.js
@@ -5,9 +5,12 @@ const srcPath = "http://localhost:3000/api/furniture/" + getFurnitureID;
 console.log(getFurnitureID);
 
 function getFurnitureDetails() {
+    console.log("début appel API");
     var getOneFurniture = new XMLHttpRequest();
     getOneFurniture.onreadystatechange = function() {
-        if (getFurnitureID.length > 0 && getOneFurniture.readyState == XMLHttpRequest.DONE && getOneFurniture.status == 200) {
+        if (getOneFurniture.readyState !== XMLHttpRequest.DONE) return;
+        if (getFurnitureID.length > 0 && getOneFurniture.status === 200) {
+            console.log("appel API terminé, création bouton");
             let Furniture = JSON.parse(getOneFurniture.response);
             console.log(Furniture);
             createElement({
@@ -50,9 +53,21 @@ function getFurnitureDetails() {
                 type: "button",
                 contentAttribution: {
                     type: "innerHTML",
-                    value: `<a href="cart.html" target="blank">Add to cart</a>`,
+                    value: `Add to cart`,
                 }
             });
+
+            createElement({
+               containerId: "product-specs",
+               type: "form",
+               contentAttribution: {
+                    type: "innerHTML",
+                    value: `<form action="choice" id="finition-form">
+                    <h3>Choose your finition varnish :</h3>
+                    <select name="finitions" id="choose-finition"></select>
+                </form>`,  
+               }  
+            })
 
             Furniture.varnish.forEach(Element => {
                 console.log(Element);
@@ -65,13 +80,37 @@ function getFurnitureDetails() {
                     }
                 });
             });
+
+            //eventlistener for shopping cart button
+            console.log("ajout eventListener");
+            const cartButton = document.getElementById("add-to-cart");
+
+            cartButton.addEventListener('click' , function(_event) {
+                const panier = JSON.parse(localStorage.getItem("cart")) || [];
+                panier.push(getFurnitureID);
+                localStorage.setItem("cart" , JSON.stringify(panier));   
+            });
+            
         }
-        else if (getOneFurniture.readyState == XMLHttpRequest.DONE && getOneFurniture.status == 404){
-            }
+        else if (getOneFurniture.status !== 200) {
+            console.log("oups cela n'existe pas");
+            createElement({
+                containerId: "product-page",
+                type: "div",
+                contentAttribution: {
+                    type: "innerHTML",
+                    value: `<h3>Oops, this product is not in our collection !<h3/>
+                    <button><a href="index.html" target="blank">Go back to homepage</a></button>`
+                }
+            })
+        }
         };
         getOneFurniture.open("GET" , srcPath);
+        console.log("envoi requête");
         getOneFurniture.send();
+        console.log("requête envoyée");
     }
+    console.log("Appel getFurnitureDetails");
     getFurnitureDetails();
 
     createElement =  function(settings) {
@@ -81,19 +120,3 @@ function getFurnitureDetails() {
         child[settings.contentAttribution.type] = settings.contentAttribution.value;
         return child;
     }
-
-    //eventlistener for shopping cart button
-
-    const cartButton = document.getElementById("add-to-cart");
-
-    cartButton.addEventListener('onclick' , function(event) {
-    console.log("hey mais ça ne marche pas !")   
-    event.createElement({
-           containerId : "my-cart",
-           type : "p",
-           contentAttribution : {
-               type : "innerHTML",
-               value : `Là c'est sensé marcher`           
-            }
-       })
-   });
