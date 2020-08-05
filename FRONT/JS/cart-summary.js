590217178462e51a2138889271c649c49ed365fc
Gwen Gyr <gwengyr@protonmail.com>
2020-08-05 15:36:57 +0200

    update cart css + btn

--- FRONT/JS/cart-summary.js
+++ FRONT/JS/cart-summary.js
@@ -33,3 +33,8 @@ if (inMyCart.length !== 0) {
     }
   });
 }
+
+const purchaseBtn = document.querySelector('button#purchase-btn')[0];
+purchaseBtn.addEventListner('click', () => {
+  window.location.href = 'http://127.0.0.1:5500/FRONT/index.html';
+});
