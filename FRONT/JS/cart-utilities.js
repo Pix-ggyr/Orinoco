8b7c0516c6dd8925ee01a03fc951a28306792de2
Gwen Gyr <gwengyr@protonmail.com>
2020-08-28 11:16:11 +0200

    cart-utilities rewritten

--- FRONT/JS/cart-utilities.js
+++ FRONT/JS/cart-utilities.js
@@ -1,7 +1,7 @@
-//enregistrer le panier;
-//fonction (class cartManager) cartManager.setCart => localStorage;
-//vider le panier;
-//récupérer le panier;
-//vérifier le panier;
-//supprimer une ligne;
-//modifier la quantité d'une ligne;
+// enregistrer le panier;
+// fonction (class cartManager) cartManager.setCart => localStorage;
+// vider le panier;
+// récupérer le panier;
+// vérifier le panier;
+// supprimer une ligne;
+// modifier la quantité d'une ligne;
